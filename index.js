fd111a0cf15e544f14066c53945f74c03682ddd8
naveen05patidar <128671277+naveen05patidar@users.noreply.github.com>
2023-08-10 16:57:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8090;
+
 const mongoose = require('mongoose');
-const config = 'mongodb+srv://naveen05patidar:naveen05patidar@cluster0.gjmvxne.mongodb.net/?retryWrites=true&w=majority';
-const port = 5000;
-const bodyPerser = require('body-parser');
-const cors = require('cors');
-const Router = require ('./route/growmark.route.js');
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const config = require('./config.js');
+const Router = require('./route/user.route.js');
+const productRouter = require('./route/product.route.js');
+
+const Product = require('./model/product.model.js');
+
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(config,{useNewUrlParser:true}).then(()=>{
-    console.log(`database is connected on ${config}`);
+mongoose.connect(config.DB,({useNewUrlParser:true})).then(()=>{
+    console.log(`mongodb is connected on ${config.DB}`);
 }).catch((err)=>{
     console.log(err);
 })
 
-app.use(cors());
-app.use(bodyPerser.urlencoded({extended:true}));
-app.use(bodyPerser.json());
 
-app.use('/api',Router);
+app.get('/np', async (req,res)=>{
+    await Product.find().then((product)=>{
+        res.status(200).json({success:true,product:product})
+    }).catch((err)=>{
+        res.status(500).json({success:false,message:"np not work"})
+    });
+})
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use('/user',Router)
+app.use('/product',productRouter)
 
 
 app.listen(port,()=>{
-    console.log(`server started on ${port}`);
+    console.log(`Server Started on ${port}`);
 })
